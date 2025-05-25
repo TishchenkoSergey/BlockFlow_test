9404af2dde2f342ed44825736af02848be7952d1
Serhii <tishchenko.seriy@gmail.com>
2025-05-25 18:42:23 +0300

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BlockFlowTestApp());
+}
+
+class BlockFlowTestApp extends StatelessWidget {
+  const BlockFlowTestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
