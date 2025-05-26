5b92431f365ba2ff9a624a573c0e08bad29c09c9
Serhii <tishchenko.seriy@gmail.com>
2025-05-26 13:07:33 +0300

    refactor: update app route dependencies

--- lib/app/route/app_route.dart
+++ lib/app/route/app_route.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/cupertino.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:go_router/go_router.dart';
+
+import 'package:get_it/get_it.dart';
 
 import 'package:block_flow/features/features.dart';
 
 import 'app_route_enum.dart';
 
 class AppRoute {
+  AppRoute({
+    required this.serviceLocator,
+  });
+
+  final GetIt serviceLocator;
+
   GoRouter build(BuildContext context) {
     return GoRouter(
       routes: [
         _buildMainPage(),
       ],
-      initialLocation: '/${Routes.mainScreen.name}',
+      initialLocation: '/${Routes.cameraScreen.name}',
     );
   }
 
   GoRoute _buildMainPage() {
     return GoRoute(
-      name: Routes.mainScreen.name,
-      path: '/${Routes.mainScreen.name}',
+      name: Routes.cameraScreen.name,
+      path: '/${Routes.cameraScreen.name}',
       pageBuilder: (context, state) => const CupertinoPage(
         child:  CameraScreen(),
       ),
--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-import 'package:block_flow/features/features.dart';
+import 'package:get_it/get_it.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
+import 'package:block_flow/app/route/route.dart';
+
+import '../../features/camera_screen/view/camera_screen.dart';
+
+class App extends StatefulWidget {
+  const App({
+    required this.serviceLocator,
+    super.key,
+  });
+
+  final GetIt serviceLocator;
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final route = AppRoute(
+    serviceLocator: widget.serviceLocator,
+  ).build(context);
 
   @override
   Widget build(BuildContext context) {
