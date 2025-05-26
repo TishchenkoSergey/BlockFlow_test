f82d2c6bf278fb8e9dcce6299b6411891d441ec4
Serhii <tishchenko.seriy@gmail.com>
2025-05-26 20:58:06 +0300

    feat: add camera mode enum for camera screen state

--- lib/features/camera_screen/bloc/camera_screen_state.dart
+++ lib/features/camera_screen/bloc/camera_screen_state.dart
@@ -1,11 +1,17 @@
 part of 'camera_screen_cubit.dart';
 
+enum CameraMode {
+  photo,
+  video,
+}
+
 @freezed
 sealed class CameraScreenState with _$CameraScreenState {
   const factory CameraScreenState({
     @Default([]) List<CameraDescription> cameras,
-    @Default(1) int selectedCameraIndex,
+    @Default(0) int selectedCameraIndex,
     @Default(false) bool isRecording,
+    @Default(CameraMode.video) CameraMode cameraMode,
     Uint8List? selectedOverlay,
     CameraController? controller,
   }) = _CameraScreenState;
