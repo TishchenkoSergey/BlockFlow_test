706599840631dcca398f6a5e02e061ab5482bdc5
Serhii <tishchenko.seriy@gmail.com>
2025-05-27 12:11:47 +0300

    refactor: delete unused packages

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,17 +1,9 @@
 import 'package:injectable/injectable.dart';
 import 'package:get_it/get_it.dart';
 
-import 'package:data/di/injection.module.dart';
-import 'package:domain/di/injection.module.dart';
-
 import 'injection.config.dart';
 
-@InjectableInit(
-  externalPackageModulesBefore: [
-    ExternalModule(DataPackageModule),
-    ExternalModule(DomainPackageModule),
-  ],
-)
+@InjectableInit()
 Future<GetIt> configureDependencies() async => GetIt.asNewInstance().init();
 
 @module
